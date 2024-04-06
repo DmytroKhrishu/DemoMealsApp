0703d7aacbe922685a1d9155dc9a578e49d66474
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-06 13:51:10 +0300

    changed background color and status bar color

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-   
-  },
+  container: {},
 });
