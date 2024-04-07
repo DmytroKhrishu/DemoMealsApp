f12e801515af6a31d6bb33fe4236349fc677dea8
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-07 14:08:06 +0300

    styled the navigator

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,22 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
+import { useEffect } from 'react';
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useEffect(() => {
+    const categoryTitle = CATEGORIES.find((item) => item.id === catId).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const mealItemProps = {
