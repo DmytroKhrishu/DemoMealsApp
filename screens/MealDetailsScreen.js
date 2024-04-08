fc23810e150901ecba949e1f365e1afae3c91bcf
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-08 14:08:14 +0300

    improved Meal Details Screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,34 +1,59 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import { useEffect } from 'react';
 
-export default function MealDetailsScreen({ route }) {
+export default function MealDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const meal = MEALS.find((meal) => meal.id === mealId);
 
+  useEffect(() => {
+    navigation.setOptions({
+      title: meal.title,
+    });
+  }, [meal, navigation]);
+  let stepCount = 0;
   return (
-    <View>
+    <ScrollView>
       <Image source={{ uri: meal.imageUrl }} style={styles.image} />
-      <Text style={styles.title}>Details {meal.title}</Text>
-      <Text style={styles.text}>
-        This meal takes {meal.duration} minutes to make
-      </Text>
-      <Text style={styles.text}>It is {meal.complexity} to make</Text>
-      <Text style={styles.text}>This meal is {meal.affordability}</Text>
-    </View>
+      <Text style={styles.title}>{meal.title}</Text>
+      <View style={styles.details}>
+        <Text style={styles.text}>Duration: {meal.duration} minutes</Text>
+        <Text style={styles.text}>Complexity: {meal.complexity}</Text>
+        <Text style={styles.text}>Cost: {meal.affordability}</Text>
+      </View>
+      <View>
+        <Text style={styles.title}>Ingredients</Text>
+        {meal.ingredients.map((ingredient) => (
+          <Text key={ingredient} style={styles.text}>
+            {ingredient}
+          </Text>
+        ))}
+        <Text style={styles.title}>Steps</Text>
+        {meal.steps.map((step) => {
+          stepCount++;
+          return (
+            <Text key={step} style={styles.text}>
+              {stepCount}: {step}
+            </Text>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: 200,
+    height: 300,
+    marginBottom: 16,
   },
   title: {
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 18,
+    fontSize: 24,
     padding: 8,
     color: 'white',
   },
@@ -37,5 +62,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 8,
     color: 'white',
+  },
+  details: {
+    borderBottomWidth: 2,
+    borderColor: 'white',
+    padding: 16,
   },
 });
